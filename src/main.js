6f74ff715bedf7e8ff6a0c1e741beb56229a4773
13588310149 <you@example.com>
2020-12-29 18:15:12 +0800

    u

--- src/main.js
+++ src/main.js
@@ -1,13 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-//swiper
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css'
+
 Vue.config.productionTip = false
-
-
-//swiper
-Vue.use(VueAwesomeSwiper, /* { default global options } */)
 
 new Vue({
   render: h => h(App),
